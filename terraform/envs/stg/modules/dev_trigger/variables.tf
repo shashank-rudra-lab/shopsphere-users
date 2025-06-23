72bd13a36b5cee7fd1ebe9c6b12df4b8409f40a7
shashank-rudra-lab <sairudra@geotab.com>
2025-06-22 20:30:14 -0400

    applied stg build

---
+++ terraform/envs/stg/modules/dev_trigger/variables.tf
@@ -0,0 +1,29 @@
+variable "app_name" {
+  type        = string
+  default     = "New-Master"
+  description = "Name of the app (used in the trigger name)"
+}
+
+variable "github_owner" {
+  type        = string
+  default     = "shashank-rudra-lab"
+  description = "GitHub organization or user name"
+}
+
+variable "github_repo_name" {
+  type        = string
+  default     = "shopsphere-users"
+  description = "GitHub repository name for the app"
+}
+
+variable "cloudbuild_path" {
+  type        = string
+  default     = "stg_cloudbuild.yaml"
+  description = "Path to the cloudbuild.yaml file in the repo"
+}
+
+variable "branch_name" {
+  type        = string
+  default     = "stg"
+  description = "Branch name to trigger builds from"
+}
